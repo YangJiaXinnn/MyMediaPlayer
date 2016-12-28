e933173f9c43e8d6ff6af1ec606ab2bdbd029ba1
unknown <782230913@qq.com>
2016-12-28 16:49:43 +0800

    Initial commit

---
+++ app/src/androidTest/java/gdmec07150644/lenovo/mymediaplayer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gdmec07150644.lenovo.mymediaplayer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
